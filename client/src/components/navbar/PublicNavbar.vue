4c5a5b3ac3649bd2b4a2eee7792df7207de4f67f
duonghm <duonghm@vtvlive.vn>
2024-03-07 17:30:06 +0700

    account

---
+++ client/src/components/navbar/PublicNavbar.vue
@@ -0,0 +1,49 @@
+<template>
+  <v-app-bar :order="0" color="#00bd7e" flat>
+    <template v-slot:prepend>
+      <div
+        style="
+          display: flex;
+          align-items: center;
+          height: 100%;
+          cursor: pointer;
+        "
+        @click="$router.replace('/')">
+        <img
+          src="./../../assets/images/logoemhnavba.png"
+          style="flex: 0; width: 30px; margin-right: 8px; margin-left: 20px" />
+        <v-app-bar-title>English Master Hub</v-app-bar-title>
+      </div>
+    </template>
+  </v-app-bar>
+</template>
+
+<script>
+import PopUpYesNo from '@/components/popup/PopUpYesNo.vue';
+import { authenticationRole } from '@/stores';
+import { mapState } from 'pinia';
+export default {
+  components: {
+    PopUpYesNo,
+  },
+  props: {
+    user: Object,
+    drawwing: Function,
+  },
+  computed: {
+    ...mapState(authenticationRole, ['clearStore']),
+  },
+  data: () => ({
+    isOpenLogout: false,
+  }),
+  methods: {
+    logout() {
+      this.isOpenLogout = false;
+      localStorage.removeItem('accessToken');
+      localStorage.removeItem('user');
+      this.clearStore();
+      this.$router.replace('/login');
+    },
+  },
+};
+</script>
