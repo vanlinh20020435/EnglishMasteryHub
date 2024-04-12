dff193f28754d587aa355c15e2ec6d526f9c633a
duonghm <duonghm@vtvlive.vn>
2024-04-12 11:40:19 +0700

    update questions

--- client/src/components/Question/index.vue
+++ client/src/components/Question/index.vue
@@ -1,18 +1,21 @@
 <template>
-    <Pronun1 v-if="question?.type === '1'" :question="question.subQuestions[0]"></Pronun1>
+  <Pronun1 v-if="question?.type === '1'" :question="question"></Pronun1>
+  <Pronun4 v-else-if="question?.type === '4'" :question="question"></Pronun4>
 </template>
 
 <script>
-import Pronun1 from '@/components/Question/Pronun1.vue'
+import Pronun1 from '@/components/Question/Pronun1.vue';
+import Pronun4 from '@/components/Question/Pronun4.vue';
 export default {
-    components: {
-        Pronun1
-    },
-    props: {
-        question: Object
-    },
-    mounted() {
-        console.log('hihihihihi: ', this.question);
-    }
-}
+  components: {
+    Pronun1,
+    Pronun4,
+  },
+  props: {
+    question: Object,
+  },
+  mounted() {
+    console.log('hihihihihi: ', this.question);
+  },
+};
 </script>
