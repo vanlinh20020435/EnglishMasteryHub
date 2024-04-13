6ce4ea4af6b90c38b00adbaf5f32c729e2359baa
LinhNV <nvl.vanlinh2002@gmail.com>
2024-04-13 23:25:19 +0700

    add writing 1 and 3

--- client/src/components/question/index.vue
+++ client/src/components/question/index.vue
@@ -2,18 +2,21 @@
   <div>
     <PronunQuestion v-if="question?.skill == 'pronunciation'" :question="question" />
     <GrammarQuestion v-if="question?.skill == 'grammar'" :question="question" />
+    <WritingQuestion v-if="question?.skill == 'writing'" :question="question" />
   </div>
 </template>
 
 <script>
 import PronunQuestion from '@/components/question/pronunQuestion/index.vue';
 import GrammarQuestion from '@/components/question/grammarQuestion/index.vue';
+import WritingQuestion from '@/components/question/writingQuestion/index.vue';
 
 export default {
   name: 'QuestionDoExam',
   components: {
     PronunQuestion,
-    GrammarQuestion
+    GrammarQuestion,
+    WritingQuestion
   },
   props: {
     question: Object,
