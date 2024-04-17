5c64ed315a3682e3e0d35e2c21597b01a16e8c6d
duonghm <duonghm@vtvlive.vn>
2024-04-17 11:45:18 +0700

    update caculate score

--- client/src/components/question/pronunQuestion/Pronun4.vue
+++ client/src/components/question/pronunQuestion/Pronun4.vue
@@ -7,21 +7,35 @@
       <div v-for="(sub, idx) in question.subQuestions">
         <p style="margin-bottom: 4px">{{ idx + 1 }}. {{ sub.content }}</p>
         <v-row v-if="$vuetify.display.smAndDown">
-          <v-col cols="12" style="padding-bottom: 0;">
-            <Audio :file="sub.files[0].url" color="success" :config="{ sound: true }"></Audio>
+          <v-col cols="12" style="padding-bottom: 0">
+            <Audio
+              :file="sub.files[0].url"
+              color="success"
+              :config="{ sound: true }"></Audio>
           </v-col>
-          <v-col cols="12" style="padding-top: 0;">
-            <v-checkbox v-for="option in sub.options" hide-details v-model="sub.selected" :label="option.option"
+          <v-col cols="12" style="padding-top: 0">
+            <v-checkbox
+              v-for="option in sub.options"
+              hide-details
+              v-model="sub.selected"
+              :label="option.option"
               :value="option.option"></v-checkbox>
           </v-col>
         </v-row>
         <v-row v-else>
           <v-col cols="6">
-            <v-checkbox v-for="option in sub.options" hide-details v-model="sub.selected" :label="option.option"
+            <v-checkbox
+              v-for="option in sub.options"
+              hide-details
+              v-model="sub.selected"
+              :label="option.option"
               :value="option.option"></v-checkbox>
           </v-col>
           <v-col cols="6">
-            <Audio :file="sub.files[0].url" color="success" :config="{ sound: true }"></Audio>
+            <Audio
+              :file="sub.files[0].url"
+              color="success"
+              :config="{ sound: true }"></Audio>
           </v-col>
         </v-row>
       </div>
@@ -30,7 +44,7 @@
 </template>
 
 <script>
-import Audio from "@/components/Audio.vue";
+import Audio from '@/components/Audio.vue';
 
 export default {
   components: {
@@ -41,39 +55,41 @@ export default {
     questionResults: Object,
   },
   mounted() {
-    this.question.subQuestions.forEach(sub => {
+    this.question.subQuestions.forEach((sub) => {
       const subquestionResult = {
         questionId: sub.questionId,
         answers: [],
         rightAnswer: null,
         score: 0,
         defaultScore: 1,
-      }
-      this.questionResults.push(subquestionResult)
-    })
+      };
+      this.questionResults.push(subquestionResult);
+    });
   },
   methods: {
     caculatePoint(data) {
       data.forEach((sub) => {
-        const qr = this.questionResults.find(q => q.questionId === sub.questionId)
-        qr.answers = [sub.selected]
+        const qr = this.questionResults.find(
+          (q) => q.questionId === sub.questionId
+        );
+        qr.answers = [sub.selected];
         if (sub.selected === sub.answers[0].answer) {
-          qr.rightAnswer = true
-          qr.score = 1
+          qr.rightAnswer = true;
+          qr.score = 1;
         } else {
-          qr.rightAnswer = false
-          qr.score = 0
+          qr.rightAnswer = false;
+          qr.score = 0;
         }
       });
-    }
+    },
   },
   watch: {
     'question.subQuestions': {
       handler: function (val) {
-        this.caculatePoint(val)
+        this.caculatePoint(val);
       },
-      deep: true
-    }
-  }
+      deep: true,
+    },
+  },
 };
 </script>
