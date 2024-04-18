cdffb32f41309b964c93ffe8e0b688152f337aed
duonghm <duonghm@vtvlive.vn>
2024-04-19 00:09:22 +0700

    update listen

--- client/src/components/question/index.vue
+++ client/src/components/question/index.vue
@@ -1,8 +1,10 @@
 <template>
   <div>
     <PronunQuestion v-if="question?.skill == 'pronunciation'" :question="question" :questionResults="questionResults" />
-    <GrammarQuestion v-if="question?.skill == 'grammar'" :question="question" :questionResults="questionResults" :indexQuestion="indexQuestion" />
+    <GrammarQuestion v-if="question?.skill == 'grammar'" :question="question" :questionResults="questionResults"
+      :indexQuestion="indexQuestion" />
     <WritingQuestion v-if="question?.skill == 'writing'" :question="question" :questionResults="questionResults" />
+    <ListeningQuestion v-if="question?.skill == 'listening'" :question="question" :questionResults="questionResults" />
   </div>
 </template>
 
@@ -10,13 +12,15 @@
 import PronunQuestion from '@/components/question/pronunQuestion/index.vue';
 import GrammarQuestion from '@/components/question/grammarQuestion/index.vue';
 import WritingQuestion from '@/components/question/writingQuestion/index.vue';
+import ListeningQuestion from '@/components/question/listeningQuestion/index.vue';
 
 export default {
   name: 'QuestionDoExam',
   components: {
     PronunQuestion,
     GrammarQuestion,
-    WritingQuestion
+    WritingQuestion,
+    ListeningQuestion
   },
   props: {
     question: Object,
